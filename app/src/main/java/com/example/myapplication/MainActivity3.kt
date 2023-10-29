9e0c8b2a1dc6b678792668b964a49f280d31edcc
valeriafern <vfernandez321@misena.edu.co>
2023-10-28 19:28:22 -0500

    paginas completas

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -1,11 +1,34 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.myapplication.databinding.ActivityMain2Binding
+import com.example.myapplication.databinding.ActivityMain3Binding
 
 class MainActivity3 : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMain3Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        binding = ActivityMain3Binding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        initIU()
     }
+
+    private  fun initIU(){
+        setupOnclickListener()
+    }
+
+    private fun setupOnclickListener() {
+        binding.finalizar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
